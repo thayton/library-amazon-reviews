e99939d1ec4192a5cb4267da15f8b76d30e1f1f2
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-02-23 21:28:01 -0500

    use advanced search page

--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import re
+import argparse
 import mechanize
 from bs4 import BeautifulSoup
 
 AMAZON_URL = 'http://www.amazon.com/gp/product/'
-LIBRARY_URL = 'https://mdpl.ent.sirsi.net/client/catalog/?'
+LIBRARY_URL = 'https://mdpl.ent.sirsi.net/client/catalog/search/advanced?'
 
 class Scraper(object):
     def __init__(self):
@@ -15,10 +16,16 @@ class Scraper(object):
                                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.63 Safari/535.7')]
 
     def rank_by_reviews(self, books):
+        ''' 
+        Sort books by rating with highest rated books first
+        '''
         newlist = sorted(books, key=lambda k: k['rating'], reverse=True) 
         return newlist
 
     def get_amazon_reviews(self, books):
+        '''
+        Get the Amazon review/rating for each book in books[]
+        '''
         for b in books:
             url = AMAZON_URL + b['isbn10']
 
@@ -32,6 +39,10 @@ class Scraper(object):
             b['rating'] = f
 
     def isbn10_check_digit(self, isbn10):
+        '''
+        Given the first 9 digits of an ISBN10 number calculate
+        the final (10th) check digit
+        '''
         i = 0
         s = 0
 
@@ -40,8 +51,8 @@ class Scraper(object):
             i += 1
 
         s = s % 11
-        s = s % 11
-        v = 11 - s
+        s = 11 - s
+        v = s % 11
         
         if v == 10:
             return 'x'
@@ -49,17 +60,32 @@ class Scraper(object):
             return str(v)
 
     def isbn13to10(self, isbn13):
+        '''
+        Convert an ISBN13 number to ISBN10 by chomping off the
+        first 3 digits, then calculating the ISBN10 check digit 
+        for the next nine digits to come up with the ISBN10 #
+        '''
         first9 = isbn13[3:][:9]
         isbn10 = first9 + self.isbn10_check_digit(first9)
         return isbn10
 
-    def search_library_books(self, q):
+    def search_library_books(self, url, q):
+        '''
+        Scrape the first page of books for the given query.
+        Calculate the ISBN10 value for each book so we can 
+        look it up on Amazon
+        '''
         books = []
 
-        self.br.open(LIBRARY_URL)
-        self.br.select_form('searchForm')
-        self.br.form['q'] = q
-        self.br.submit('searchButton')
+        def select_form(form):
+            return form.attrs.get('id', None) == 'advancedSearchForm'
+
+        self.br.open(url)
+        self.br.select_form(predicate=select_form)
+        self.br.form['allWordsField'] = q
+        self.br.form['formatTypeDropDown'] = ['BOOK']
+        self.br.form['languageDropDown'] = ['ENG']
+        self.br.submit()
 
         s = BeautifulSoup(self.br.response().read())
         x = {'class': 'isbnValue'}
@@ -81,8 +107,8 @@ class Scraper(object):
 
         return books
 
-    def scrape(self, q='javascript'):
-        books = self.search_library_books(q=q)
+    def scrape(self, url, q):
+        books = self.search_library_books(url=url, q=q)
         self.get_amazon_reviews(books)
         books = self.rank_by_reviews(books)
 
@@ -90,5 +116,10 @@ class Scraper(object):
             print b['rating'], b['title']
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Search Sirsi catalogue. Return results ranked by Amazon rating.")
+    parser.add_argument("-u", "--url",   help="Sirsi url", required=True)
+    parser.add_argument("-q", "--query", help="Query string", required=True)
+    args = parser.parse_args()
+
     scraper = Scraper()
-    scraper.scrape()
+    scraper.scrape(args.url, args.query)
